2a29c5cd4262a450b6d16d453a3f1d3e03961810
Thang Ngoc <tnthang2910@gmail.com>
2014-11-23 22:14:28 +0700

    init commit

---
+++ TipCalculatorModel.swift
@@ -0,0 +1,43 @@
+//
+//  SimpleCalculatorModel.swift
+//  SimpleCalculator
+//
+//  Created by Thang Ngoc Tran on 11/23/14.
+//  Copyright (c) 2014 Thang Ngoc Tran. All rights reserved.
+//
+
+import Foundation
+
+class TipCalculatorModel {
+    
+    var total: Double
+    var taxPct: Double
+    var subtotal: Double {
+        get {
+            return total / (taxPct + 1)
+        }
+    }
+    
+    init(total:Double, taxPct:Double) {
+        self.total = total
+        self.taxPct = taxPct
+    }
+    
+    func calcTipWithTipPct(tipPct:Double) -> Double {
+        return subtotal * tipPct
+    }
+    
+    func returnPossibleTips() -> [Int: Double] {
+        
+        let possibleTipsInferred = [0.15, 0.18, 0.20]
+        let possibleTipsExplicit:[Double] = [0.15, 0.18, 0.20]
+        
+        var retval = [Int: Double]()
+        for possibleTip in possibleTipsInferred {
+            let intPct = Int(possibleTip*100)
+            retval[intPct] = calcTipWithTipPct(possibleTip)
+        }
+        return retval
+        
+    }
+}
